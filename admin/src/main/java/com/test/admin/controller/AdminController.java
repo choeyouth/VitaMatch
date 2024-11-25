d425cab84513b05524c7ae4d9114b6ac03fab0ee
chimy2 <premet@naver.com>
2024-11-25 19:58:40 +0900

    Add admin view

--- admin/src/main/java/com/test/admin/controller/AdminController.java
+++ admin/src/main/java/com/test/admin/controller/AdminController.java
@@ -1,8 +1,30 @@
 package com.test.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class AdminController {
 
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "page/login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin() {
+
+		return "page/login";
+	}
+	
 }
