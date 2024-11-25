7e1fa4d0215a5ea02053c0204c9087c6382e65bd
yujin0510 <yuyujinkim65@gmail.com>
2024-11-25 23:28:26 +0900

    Update recommend controller, repository, css

--- nutri/src/main/java/com/test/nutri/controller/RecommendController.java
+++ nutri/src/main/java/com/test/nutri/controller/RecommendController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.test.nutri.entity.News;
+import com.test.nutri.entity.vwDailyRecommend;
 import com.test.nutri.entity.vwGenderAgeRecommend;
 import com.test.nutri.entity.vwHealthRecommend;
+import com.test.nutri.entity.vwOrganRecommend;
 import com.test.nutri.repository.DailyRecommendRepository;
 import com.test.nutri.repository.GenderAgeRecommendRepository;
 import com.test.nutri.repository.HealthRecommendRepository;
+import com.test.nutri.repository.OrganRecommendRepository;
 import com.test.nutri.service.NewsService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +26,8 @@ public class RecommendController {
 	
 	private final GenderAgeRecommendRepository genderAgeRecommendRepository;
 	private final HealthRecommendRepository healthRecommendRepository;
+	private final OrganRecommendRepository organRecommendRepository;
+	private final DailyRecommendRepository dailyRecommendRepository;
 	
 	@GetMapping("/survey")
 	public String survey(Model model) {
@@ -36,10 +41,14 @@ public class RecommendController {
 		
 		List<vwGenderAgeRecommend> galist = genderAgeRecommendRepository.listAll(5);
 		List<vwHealthRecommend> hlist = healthRecommendRepository.listAll(1);
+		List<vwOrganRecommend> olist = organRecommendRepository.listAll(2);
+		List<vwDailyRecommend> dlist = dailyRecommendRepository.listAll(1);
 		
 		
 		model.addAttribute("galist", galist);
 		model.addAttribute("hlist", hlist);
+		model.addAttribute("olist", olist);
+		model.addAttribute("dlist", dlist);
 		
 		return "page/recommend";
 	}
--- nutri/src/main/java/com/test/nutri/repository/DailyRecommendRepository.java
+++ nutri/src/main/java/com/test/nutri/repository/DailyRecommendRepository.java
@@ -1,5 +1,15 @@
 package com.test.nutri.repository;
 
-public class DailyRecommendRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.test.nutri.entity.vwDailyRecommend;
+
+public interface DailyRecommendRepository extends JpaRepository<vwDailyRecommend, Long> {
+
+	@Query("select d from vwDailyRecommend d where d.dailySeq = ?1")
+	List<vwDailyRecommend> listAll(int dailySeq);
 
 }
