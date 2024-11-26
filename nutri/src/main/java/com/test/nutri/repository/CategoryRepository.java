23e225879cb073f0177dcf75fdbd0d61c842de5c
JayChoi97 <chol6245@naver.com>
2024-11-26 11:10:40 +0900

    category

--- nutri/src/main/java/com/test/nutri/repository/CategoryRepository.java
+++ nutri/src/main/java/com/test/nutri/repository/CategoryRepository.java
@@ -1,7 +1,5 @@
 package com.test.nutri.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.test.nutri.entity.Category;
+import com.test.nutri.entity.HealthCategory;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 	
 	 @Query("SELECT c FROM Category c WHERE c.categoryName = :categoryName")
 	 Page<Category> findByCategory(@Param("categoryName") String categoryName, Pageable pageable);
+
+	 @Query("SELECT a From HealthCategory a WHERE a.health = :health")
+	 Page<HealthCategory> findByHealth(@Param("health") String category, Pageable pageable);
 	
 }
