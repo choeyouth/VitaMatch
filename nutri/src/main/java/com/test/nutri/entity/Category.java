3a072ffbb9fc265df02517deb30a8aeb61933186
JayChoi97 <chol6245@naver.com>
2024-11-29 11:16:13 +0900

    category css edit

--- nutri/src/main/java/com/test/nutri/entity/Category.java
+++ nutri/src/main/java/com/test/nutri/entity/Category.java
@@ -12,6 +12,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * Category 엔티티 클래스.
+ * 데이터베이스의 "categories" 테이블과 매핑되며, 영양제 및 건강 제품의 정보를 담고 있음.
+ */
 @Entity
 @Table(name = "categories")
 @Getter
@@ -21,19 +25,39 @@ import lombok.ToString;
 @ToString
 public class Category {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
-	private Long id;
-	
-	@Column(name = "productImage")
-	private String productImage;
-	
-	@Column(name = "productName")
-	private String productName;
-	
-	@Column(name = "companyName")
-	private String productcompany;
-	
-	@Column(name = "categoryName")
-	private String categoryName;
+    /**
+     * 기본 키 (Primary Key).
+     * 데이터베이스에서 자동 생성되는 ID 값.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+    private Long id;
+
+    /**
+     * 제품 이미지 URL을 저장.
+     * 테이블의 "productImage" 열과 매핑.
+     */
+    @Column(name = "productImage")
+    private String productImage;
+
+    /**
+     * 제품 이름을 저장.
+     * 테이블의 "productName" 열과 매핑.
+     */
+    @Column(name = "productName")
+    private String productName;
+
+    /**
+     * 제품 제조 회사 이름을 저장.
+     * 테이블의 "companyName" 열과 매핑.
+     */
+    @Column(name = "companyName")
+    private String productcompany;
+
+    /**
+     * 제품의 카테고리 이름을 저장.
+     * 테이블의 "categoryName" 열과 매핑.
+     */
+    @Column(name = "categoryName")
+    private String categoryName;
 }
