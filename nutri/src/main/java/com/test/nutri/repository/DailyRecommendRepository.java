f251512f68795261a27c44a61c6099914c760ce4
yujin0510 <yuyujinkim65@gmail.com>
2024-12-02 10:07:12 +0900

    Add Recommend Document

--- nutri/src/main/java/com/test/nutri/repository/DailyRecommendRepository.java
+++ nutri/src/main/java/com/test/nutri/repository/DailyRecommendRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.test.nutri.entity.VwDailyRecommend;
 
+/**
+ * DailyRecommendRepository는 데이터베이스 작업을 수행하는 리포지토리 인터페이스입니다.
+ * VwDailyRecommend 엔티티에 대한 기본적인 CRUD 작업.
+ * @author Yujin Kim
+ */
 public interface DailyRecommendRepository extends JpaRepository<VwDailyRecommend, Long> {
 
 	@Query("select d from VwDailyRecommend d where d.dailySeq = ?1")
