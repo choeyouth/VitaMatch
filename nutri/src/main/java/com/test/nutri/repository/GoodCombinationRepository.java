239244dff0fb4dc2d0feb52002d35ef845ac3690
FJHGF <fjhgf@naver.com>
2024-12-02 09:35:40 +0900

    Add Combination document code

--- nutri/src/main/java/com/test/nutri/repository/GoodCombinationRepository.java
+++ nutri/src/main/java/com/test/nutri/repository/GoodCombinationRepository.java
@@ -7,6 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.test.nutri.entity.VwGoodCombination;
 
+/**
+ * GoodCombinationRepository
+ * VwGoodCombination 엔티티를 처리하는 JPA 리포지토리 인터페이스
+ * 기본적인 CRUD 작업을 진행
+ * @author bohwa Jang
+ */
 public interface GoodCombinationRepository extends JpaRepository<VwGoodCombination, Long>{
 
 	List<VwGoodCombination> findByIngredientSeq(String ingredientSeq);
